3ca0bc34981ea439adc020e05f88efff00be2e39
Nitin Chalana <121996387+nitin-011@users.noreply.github.com>
2024-10-30 15:33:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ import cors from 'cors';
 import userRoutes from './routes/user.route.js';
 import carrierRoutes from './routes/carrier.route.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config({path: './config/config.env'});
 const app = express();
 
@@ -12,6 +18,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 // Database Connection
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
